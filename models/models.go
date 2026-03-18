a506236075ea14829d7299e001084575587027ab
ChaitanyaSai-Meka <chaitanya.s.meka@gmail.com>
2026-03-18 18:01:57 +0530

    updated the models

--- models/models.go
+++ models/models.go
@@ -3,26 +3,31 @@ package models
 type User struct {
 	UserID string
 	UserName string
-	Balance int
 }
 
 type Group struct {
 	GroupID string
 	GroupName string
-	MembersIDs []string
+}
+
+type GroupMember  struct {
+	GroupID string
+	UserID string
 }
 
 type Expense struct {
 	ExpenseID string
-	Amount int
-	Service string
-	PaidBy string
+	Amount float32
+	Description string
+	PaidByUserID string
 	GroupID string
 }
 
 type Split struct {
+	ExpenseID string
 	UserID string
-	Amount int
+	Amount float32
+	Settled bool
 }
 
 
