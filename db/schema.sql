8fccd144fcea2d4665507651991bf585bd6ee44d
ChaitanyaSai-Meka <chaitanya.s.meka@gmail.com>
2026-03-18 19:05:43 +0530

    Refactor database schema: rename User table to Users, update foreign key references, and change Amount type to INTEGER

--- db/schema.sql
+++ db/schema.sql
@@ -1,38 +1,38 @@
-CREATE TABLE User (
-        UserID INTEGER PRIMARY KEY,
-        UserName TEXT   
+CREATE TABLE Users (
+    UserID INTEGER PRIMARY KEY,
+    UserName TEXT   
 );
 
 CREATE TABLE Groups (
-        GroupID INTEGER PRIMARY KEY,
-        GroupName TEXT  
+    GroupID INTEGER PRIMARY KEY,
+    GroupName TEXT  
 );
 
 CREATE TABLE GroupMember (
-        GroupID INTEGER,   
-        UserID INTEGER,   
-        PRIMARY KEY (GroupID , UserID),
-        FOREIGN KEY (GroupID) REFERENCES Groups(GroupID),
-        FOREIGN KEY (UserID) REFERENCES User(UserID)
+    GroupID INTEGER,   
+    UserID INTEGER,   
+    PRIMARY KEY (GroupID , UserID),
+    FOREIGN KEY (GroupID) REFERENCES Groups(GroupID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE Expense (
 	ExpenseID INTEGER PRIMARY KEY,
-	Amount REAL,
+	Amount INTEGER,
 	Description TEXT,
 	PaidByUserID INTEGER,
 	GroupID INTEGER,
-	FOREIGN KEY (PaidByUserID) REFERENCES User(UserID),
+	FOREIGN KEY (PaidByUserID) REFERENCES Users(UserID),
 	FOREIGN KEY (GroupID) REFERENCES Groups(GroupID)
 );
 
 CREATE TABLE Split (
     ExpenseID INTEGER,
 	UserID INTEGER,
-	Amount REAL,
+	Amount INTEGER,
     PRIMARY KEY (ExpenseID, UserID),
-	Settled BOOLEAN,
+	Settled BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY (ExpenseID) REFERENCES Expense(ExpenseID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
