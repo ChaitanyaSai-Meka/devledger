d17e6408244d71a0c96c88c6353fcdef492f9dc1
ChaitanyaSai-Meka <chaitanya.s.meka@gmail.com>
2026-04-01 09:56:01 +0530

    fix: add JSON tags to struct fields for proper serialization

--- models/models.go
+++ models/models.go
@@ -1,35 +1,33 @@
 package models
 
 type User struct {
-	UserID int
-	UserName string
+	UserID    int    `json:"user_id"`
+	UserName  string `json:"username"`
+	DeletedAt string `json:"deleted_at,omitempty"`
 }
 
 type Group struct {
-	GroupID int
-	GroupName string
+	GroupID   int    `json:"group_id"`
+	GroupName string `json:"group_name"`
 }
 
 type GroupMember struct {
-	GroupID int
-	UserID int
+	GroupID int `json:"group_id"`
+	UserID  int `json:"user_id"`
 }
 
 type Expense struct {
-	ExpenseID int
-	Amount int64
-	Description string
-	PaidByUserID int
-	GroupID int
-	CreatedAt string
+	ExpenseID    int64  `json:"expense_id"`
+	Amount       int64  `json:"amount"`
+	Description  string `json:"description"`
+	PaidByUserID int    `json:"paid_by_user_id"`
+	GroupID      int    `json:"group_id"`
+	CreatedAt    string `json:"created_at"`
 }
 
 type Split struct {
-	ExpenseID int
-	UserID int
-	Amount int64
-	Settled bool
+	ExpenseID int64 `json:"expense_id"`
+	UserID    int   `json:"user_id"`
+	Amount    int64 `json:"amount"`
+	Settled   bool  `json:"settled"`
 }
-
-
-
